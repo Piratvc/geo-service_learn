6bd6ef65ce2d184bfbbe0071b1d156e8c486f109
Piratvc <piratvc@gmail.com>
2022-04-19 17:48:57 +0300

    commit

---
+++ src/test/java/TestSender.java
@@ -0,0 +1,93 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
+import ru.netology.sender.MessageSender;
+import ru.netology.sender.MessageSenderImpl;
+import java.util.HashMap;
+
+
+public class TestSender {
+
+    @Test
+   public void testRussiaIp() {
+
+        GeoService geoService = Mockito.mock(GeoService.class);
+        Mockito.when(geoService.byIp("172.152.22.11"))
+                .thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
+
+        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
+        Mockito.when(localizationService.locale(Country.RUSSIA))
+                .thenReturn( "Добро пожаловать");
+
+        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+
+        HashMap testMap = new HashMap();
+        testMap.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.152.22.11");
+
+        String actual = messageSender.send(testMap);
+        String expected = "Добро пожаловать";
+
+        Assertions.assertEquals(expected,
+                actual);
+    }
+    @Test
+    public void testUSAIp() {
+        GeoService geoService = Mockito.mock(GeoService.class);
+        Mockito.when(geoService.byIp("96.152.22.11"))
+                .thenReturn(new Location("New York", Country.USA, " 10th Avenue", 32));
+
+        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
+        Mockito.when(localizationService.locale(Country.USA))
+                .thenReturn( "Welcome");
+
+        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+
+        HashMap testMap = new HashMap();
+        testMap.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.152.22.11");
+
+        String actual = messageSender.send(testMap);
+        String expected = "Welcome";
+
+        Assertions.assertEquals(expected,
+                actual);
+
+    }
+
+    @Test
+    public void testIpCountry() {
+        GeoService geoService = new GeoServiceImpl();
+
+        Location actual = geoService.byIp("96.222.12.1");
+        Location expected = new Location(("New York"), Country.USA, null, 0);
+
+        Assertions.assertEquals(expected.getCountry(), actual.getCountry());
+
+    }
+
+    @Test
+    public void testTextReturnRus() {
+        LocalizationService localizationService = new LocalizationServiceImpl();
+
+        String actual = localizationService.locale(Country.RUSSIA);
+        String expected = "Добро пожаловать";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTextReturnUSA() {
+        LocalizationService localizationService = new LocalizationServiceImpl();
+
+        String actual = localizationService.locale(Country.USA);
+        String expected = "Welcome";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
